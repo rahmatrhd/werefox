df24d14b38d05a306652279fb176703ad01a3e4f
tamastro <rpratama45@gmail.com>
2017-09-07 22:23:06 +0700

    room

--- client/src/components/AddRoom.vue
+++ client/src/components/AddRoom.vue
@@ -7,27 +7,33 @@
             <img src="../assets/plus.png" />
           </div>
           <div class="card-block">
-            <h4 class="card-title">Room Name</h4>
+            <h4 class="card-title">Add Town</h4>
           </div>
         </div>
       </div>
-      <div class="cardo col-sm-12 col-md-3">
+      <div class="cardo col-sm-12 col-md-3" v-for="room in rooms">
         <div class="box">
           <div class="img-wrapper">
             <img src="../assets/wolfie.png" />
           </div>
           <div class="card-block">
-            <h4 class="card-title">Room Name</h4>
+            <h4 class="card-title">Town Name</h4>
+            <h3 class="card-title"><strong>{{room.name}}</strong></h3>
+            <h4 class="card-title">Population: {{Object.keys(room.member).length}}</h4>
           </div>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'addroom'
+  name: 'addroom',
+  firebase () {
+    return {
+      rooms: this.$db.ref('rooms')
+    }
+  }
 }
 </script>
